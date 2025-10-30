7f78fd0ebc5a4ae124d8efe01413427dc1c46a4d
minase <minasegirma5@gmail.com>
2025-10-30 18:02:38 +0100

    axios

--- src/axiosconfig.js
+++ src/axiosconfig.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
-const API_BASE_URL = "https://new-evangadi-forum-final-1.onrender.com";
-
-export const api = axios.create({
-  baseURL: API_BASE_URL,
-  withCredentials: true, // if you’re using cookies or sessions
+const axiosInstance = axios.create({
+  baseURL:
+    import.meta.env.VITE_API_URL ||
+    "https://new-evangadi-forum-final-1.onrender.com/api",
+  withCredentials: true,
 });
 
-export default api;
+export default axiosInstance;
