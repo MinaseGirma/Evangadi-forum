01082f2569ea4c111bb49fddbf1b3bdcc5be3823
minase <minasegirma5@gmail.com>
2025-10-30 17:26:51 +0100

    axios server

--- src/axiosconfig.js
+++ src/axiosconfig.js
@@ -1,5 +1,10 @@
-import axios from 'axios'
-const axiosInstance =axios.create({
-    baseURL:'http://localhost:5000/api'
+import axios from "axios";
+
+const axiosInstance = axios.create({
+  baseURL:
+    import.meta.env.VITE_API_URL ||
+    "https://new-evangadi-forum-final-1.onrender.com/api",
+  withCredentials: true,
 });
+
 export default axiosInstance;
